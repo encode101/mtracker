45af46261dda51a746aa85cdbfa0575b873c53be
encode101 <rahul_mishra@neovasolutions.com>
2015-09-04 19:05:21 +0530

    interim commit

--- www/controllers/DashboardController.js
+++ www/controllers/DashboardController.js
@@ -2,23 +2,32 @@ angular.module('DashboardController', [])
 .controller('DashboardController', function($scope, $cordovaSms) {
   $scope.title = "My Dashboard";
 
+  var appBucket = Kii.bucketWithName("Location");
+
   document.addEventListener("deviceready", function () {
 
         var smsList = [];
         var interceptEnabled = false;
 
 
-    /*WifiWizard.setWifiEnabled(true, function(){
+    WifiWizard.setWifiEnabled(true, function(){
       document.getElementById('info').innerHTML = "Connected To Wifi...";
     }, function(error){
         cosole.log("Error : "+error);
-    });*/
+    });
 
      document.addEventListener('onSMSArrive', function(e){
         var data = e.data;
         smsList.push( data );        
         document.getElementById('info').innerHTML = JSON.stringify( data );        
       });
+
+     cordova.plugins.backgroundMode.setDefaults({
+        resume: false,
+        silent: true
+     })
+
+     cordova.plugins.backgroundMode.enable();
 
 
     // device Ready Ends
